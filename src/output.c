dbb3000790cf386284b9c26bfbea04d3a57ef136
MrDaPoyo <wiiuforever25@gmail.com>
2026-03-01 20:22:10 +0100

    just in case

--- src/output.c
+++ src/output.c
@@ -1,16 +1,25 @@
 #include <unistd.h>
+#include <stdio.h>
 
 void editorDrawRows() {
     int y;
     for (y = 0; y<24; y++) {
-        write(STDOUT_FILENO, "~\r\n", 3); // 3 bytes, remember.
+        char c = '~';
+
+        // if (y%2) c = '-';
+
+        char buffer[4];
+        snprintf(buffer, sizeof(buffer), "%c\r\n", c);
+        write(STDOUT_FILENO, buffer, 3);
+
+        // write(STDOUT_FILENO, "~\r\n", 3); // 3 bytes, remember.
     }
 }
 
 void editorRefreshScreen() {
     write(STDOUT_FILENO, "\x1b[2J", 4); // \x1b is the escape character. J clears the entire (2J) screen
     write(STDOUT_FILENO, "\x1b[H", 3); // Move cursor to 1;1 (default)
-    
+
     editorDrawRows();
 
     write(STDOUT_FILENO, "\x1b[H", 3); // 3 bytes again.
